265e7b498390bbaf5c6dc06e3a9e4ecb288685a1
jullfie <126818076+jullfie@users.noreply.github.com>
2024-05-14 02:01:55 +0300

    Update Prey.cpp

--- Prey.cpp
+++ Prey.cpp
@@ -1,12 +1,13 @@
-#include "Prey.h"
+#include "Prey.h" // вкл заголовочного файла для объявл
 #include <stdexcept> //директива препроцессора, которая включает заголовочный файл <stdexcept>, содержащий определение стандартных исключений для C++
 
-void Prey::move(int type) {
-    switch (type) {
-        case 0:
-            if(pos.getX() + 1 > 29) throw std::runtime_error("out of bound");
-            pos.setX(pos.getX() + 1);
-            break;
+void Prey::move(int type) { //определение метода move() класса рrey, который принимает тип движения в качестве аргумента
+    switch (type) { // начало оператора switch для различных вариантов типа движения
+        case 0://ести тип движ равен 0
+            if(pos.getX() + 1 > 29) throw std::runtime_error("out of bound"); // проверка не выходит ли добыча за границы поля по оси х
+            pos.setX(pos.getX() + 1); //увеличим  координаты х позиции добычи на 1
+            break; // завершение случ 0 
+// для каждого варианта проверяется выход за границы поля по оси X и Y, а затем происходит соответствующее перемещение
         case 1:
             if (pos.getX() - 1 < 0) throw std::runtime_error("out of bound");
             pos.setX(pos.getX() - 1);
@@ -46,14 +47,14 @@ void Prey::move(int type) {
     }
 }
 
-void Prey::initialize() {
-    pos.initialize();
+void Prey::initialize() { // опред метода initialize() класса Prey без параметров
+    pos.initialize(); //вызов метода initialize() для инициализации позиции добычи
 }
 
-void Prey::initialize(Point2D point) {
-    pos.initialize(point);
+void Prey::initialize(Point2D point) { // определение метода initialize() класса Prey с параметром типа Point2D
+    pos.initialize(point); // вызов метода initialize() для инициализации позиции добычи с заданными координатами
 }
 
-const Point2D &Prey::getPos() const {
-    return pos;
+const Point2D &Prey::getPos() const { // определение метода getPos() класса Prey, возвращающего константную ссылку на позицию добычи
+    return pos; // возврат константной ссылки на объект Point2D, представляющий позицию добычи
 }
