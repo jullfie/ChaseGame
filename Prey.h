a2989623424a9c19f0d8745ebe42e4a5f60b6c6f
jullfie <126818076+jullfie@users.noreply.github.com>
2024-05-14 01:50:17 +0300

    Update Prey.h

--- Prey.h
+++ Prey.h
@@ -1,4 +1,4 @@
-#ifndef CHASEGAME_PREY_H
+#ifndef CHASEGAME_PREY_H //директива препроцессора ifndef проверяет не определен ли  уже символ
 #define CHASEGAME_PREY_H
 
 
@@ -7,14 +7,14 @@
 
 class Prey{
 private:
-    Point2D pos;
+    Point2D pos; // объявл закрытого члена класса prey - объекта типа point2D для хранения позиции добычи
 public:
     void initialize();
-    void initialize(Point2D point);
+    void initialize(Point2D point); //объявл методов initialize() класса prey для инициализации добычи без параметров и с заданнной позицией
 
-    const Point2D &getPos() const;
+    const Point2D &getPos() const; // объявл метода getPos() класса prey- возвращ константную ссылку на позицию добычи
 
-    void move(int type);
+    void move(int type); //объявл метода move() класса prey - для перемещ добычи в соответствии с заданным типом движ
 };
 
 
