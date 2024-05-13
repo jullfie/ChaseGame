e531616bcad3e58747d971bb6bd997b22b0c20f7
jullfie <126818076+jullfie@users.noreply.github.com>
2024-05-14 01:31:26 +0300

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,27 +1,27 @@
 #ifndef CHASEGAME_CHARACTER_H
 #define CHASEGAME_CHARACTER_H
-
+// проверяем что CHASEGAME_CHARACTER_H не определен, если соответствует - код выполнен , нет - скип
 
 #include <string>
-#include "Predator.h"
-#include "Prey.h"
+#include "Predator.h" // вкл заголовочного файла для объявл класса predator
+#include "Prey.h" вкл заголовочного файла для объявл класса prey
 
-class Character {
+class Character { //объявл класса
 
-private:
-    std::string type;
-    Predator predator;
+private: // члены класса будут приватными-доступны только внутри класса
+    std::string type; // приватное поле для хранения типа перса
+    Predator predator; 
     Prey prey;
-public:
-    const std::string &getType() const;
-    void initialize();
-    void initialize(Point2D point);
-    void setType(const std::string &type);\
-    void move(int type);
-    void move(int type, int distance);
-    Point2D getPos();
+public: // члены класса будут публичными-доступны извне класса
+    const std::string &getType() const; // объявл метода класса для получения типа перса
+    void initialize(); //объявл метода класса для инициализации перса
+    void initialize(Point2D point); //объявл метода класса для инициализации перса с передачей объекта 
+    void setType(const std::string &type);// объявл метода класса для установки типа перса
+    void move(int type); //объявл метода класса для перемещения перса по заданному типу
+    void move(int type, int distance); //объявл метода класса для перемещения перса по заданному типу на указанное расстояние
+    Point2D getPos(); //объявл метода класса для получения позиции перса
 
 };
 
-
+//заверш определения загол файла 
 #endif //CHASEGAME_CHARACTER_H
